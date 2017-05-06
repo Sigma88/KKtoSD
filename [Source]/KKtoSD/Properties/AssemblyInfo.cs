f36fa3ec04850d867f6917eed69050409b23b043
Sigma88 <sigmamodexpansions@outlook.com>
2017-05-06 04:24:14 +0200

    dev

---
+++ [Source]/KKtoSD/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("KKtoSD")]
+[assembly: AssemblyDescription("KKtoSD")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: KSPAssembly("KKtoSD", 1, 0)]
+[assembly: KSPAssemblyDependency("SigmaDimensions", 1, 0)]
+[assembly: AssemblyVersion("1.0.0")]
+
