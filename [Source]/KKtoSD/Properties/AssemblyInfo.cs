66b0413c9696d62a58b051b38705791cac7073dd
Sigma88 <sigmamodexpansions@outlook.com>
2017-05-30 10:12:25 +0200

    assembly info, version, recompile

--- [Source]/KKtoSD/Properties/AssemblyInfo.cs
+++ [Source]/KKtoSD/Properties/AssemblyInfo.cs
@@ -1,11 +1,11 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("KKtoSD")]
-[assembly: AssemblyDescription("KKtoSD")]
+[assembly: AssemblyDescription("Automatically loads KK groups into SD")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Sigma88")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Copyright (C) Sigma88 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: KSPAssembly("KKtoSD", 1, 0)]
